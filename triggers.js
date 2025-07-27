182b571d575d1c798fc9deda080e1923d1d76ea3
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2025-07-27 07:21:54 -0600

    Stock summary is only sent during the weekdays

--- triggers.js
+++ triggers.js
@@ -1,34 +1,38 @@
-function daily(){
-    send_daily_stock_summary_message(Object.keys(CHAT_TO_USER)[0],title = "<b>📫    📰  Daily Pre-Market Overview  📰    📫</b>\n", true);
+function daily() {
+  const today = new Date().getDay(); // 0 = Sunday, 1 Monday, ...
+
+  if (today >= 1 && today <= 5) {
+    // Monday to Friday
+    send_daily_stock_summary_message(
+      Object.keys(CHAT_TO_USER)[0],
+      (title = "<b>📬    📰  Daily Pre-Market Overview  📰    📬</b>\n"),
+      true
+    );
+  }
 }
 
-function send_daily_stock_summary_message(chatId, title, phrase = false){
-    let tracked_stocks = readDataFromSheet("TRACK", "edit_sheet").slice(2);
-    let raw_currencies = readDataFromSheet("CURRENCIES", "edit_sheet").slice(1);
-    let stocks = [];
-    let currencies = [];
-    for (const stock of tracked_stocks) {
-      stocks.push(new Stock(stock[0], (Number(stock[3]) * 100).toFixed(2)));
-    }
-    for (const currency of raw_currencies) {
-      currencies.push(
-        new Currency(
-          Number(currency[2]),
-          currency[1],
-          CURRENCY_EMOJIS_MAP[currency[1]] ?? ""
-        )
-      );
-    }
-
-    sendMessage(
-      chatId,
-      stock_summary_message(
-        title,
-        currencies,
-        stocks,
-        phrase
-      ),
-      true,
-      "HTML"
+function send_daily_stock_summary_message(chatId, title, phrase = false) {
+  let tracked_stocks = readDataFromSheet("TRACK", "edit_sheet").slice(2);
+  let raw_currencies = readDataFromSheet("CURRENCIES", "edit_sheet").slice(1);
+  let stocks = [];
+  let currencies = [];
+  for (const stock of tracked_stocks) {
+    stocks.push(new Stock(stock[0], (Number(stock[3]) * 100).toFixed(2)));
+  }
+  for (const currency of raw_currencies) {
+    currencies.push(
+      new Currency(
+        Number(currency[2]),
+        currency[1],
+        CURRENCY_EMOJIS_MAP[currency[1]] ?? ""
+      )
     );
+  }
+
+  sendMessage(
+    chatId,
+    stock_summary_message(title, currencies, stocks, phrase),
+    true,
+    "HTML"
+  );
 }
